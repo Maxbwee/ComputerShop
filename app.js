0243c91df5f00d6360745ac2b1b4429a05260563
Maxbwee <maxbowen1234@gmail.com>
2023-01-12 15:51:18 +0200

    Started working on website HTML, CSS and Js

---
+++ app.js
@@ -0,0 +1,10 @@
+const laptopsElement = document.getElementById("laptops");
+const workBalanceElement = document.getElementById("work-balance");
+const bankBalanceElement = document.getElementById("bank-balance");
+const workButtonElement = document.getElementById("work-btn");
+const bankButtonElement = document.getElementById("bank-btn");
+const loanButtonElement = document.getElementById("loan-btn");
+
+let laptops = []; 
+let workBalance = 0;
+let bankBalance = 0;
