e3ab275996d8e80ae7a63a8c000a6de8cd3ea242
Maxbwee <maxbowen1234@gmail.com>
2023-01-12 16:21:42 +0200

    Added section for laptop details

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const bankBalanceElement = document.getElementById("bank-balance");
 const workButtonElement = document.getElementById("work-btn");
 const bankButtonElement = document.getElementById("bank-btn");
 const loanButtonElement = document.getElementById("loan-btn");
+const buyButtonElement = document.getElementById("buy-btn");
+const laptopNameElement = document.getElementById("laptop-name");
+const laptopDescElement = document.getElementById("laptop-desc");
+const laptopPriceElement = document.getElementById("laptop-price");
+
 
 let laptops = []; 
 let workBalance = 0;
