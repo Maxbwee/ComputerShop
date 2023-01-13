e0a3bd63af9544a6190a2c9241de08b40b90a6ec
Maxbwee <maxbowen1234@gmail.com>
2023-01-13 16:17:04 +0200

    Listing laptops on the webpage

--- app.js
+++ app.js
@@ -13,3 +13,20 @@ const laptopPriceElement = document.getElementById("laptop-price");
 let laptops = []; 
 let workBalance = 0;
 let bankBalance = 0;
+
+fetch("https://hickory-quilled-actress.glitch.me/computers")
+    .then(response => response.json())
+    .then(data => laptops = data)
+    .then(laptops => addLaptops(laptops))
+
+const addLaptops = (laptops) => {
+    laptops.forEach(x => addLaptopDropDown(x));
+}
+
+const addLaptopDropDown = (laptop) => {
+    const laptopElement = document.createElement("option");
+    laptopElement.value = laptop.id;
+    laptopElement.appendChild(document.createTextNode(laptop.title));
+    laptopsElement.appendChild(laptopElement);
+
+}
